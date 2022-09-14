bcf7cd13d2d2d9ce750ae63b68c34f8e9084ccab
Ruchika Jain <31808822+ruchika-jain@users.noreply.github.com>
2022-09-14 10:26:37 +0000

    Add release.yml

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,8 @@ async function run(): Promise<void> {
     const ms: string = core.getInput('milliseconds')
     core.debug(`Waiting ${ms} milliseconds ...`) // debug is only output if you set the secret `ACTIONS_STEP_DEBUG` to true
 
+    core.info("Woohoo!! Action executed successfully..")
+
     core.debug(new Date().toTimeString())
     await wait(parseInt(ms, 10))
     core.debug(new Date().toTimeString())
